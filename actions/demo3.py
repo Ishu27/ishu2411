01f2efc48fbdeb932ee5f0d6367e4b45a212e492
samarp <samarp.gupta18@gmail.com>
2019-08-21 09:16:13 +0000

    On branch master

---
+++ actions/demo3.py
@@ -0,0 +1,11 @@
+import sys
+
+from st2common.runners.base_action import Action
+
+class MyEchoAction(Action):
+    def run(self, message):
+        print(message)
+
+        if message == 'working':
+            return (True, message)
+        return (False, message)
