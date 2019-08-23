9051fee6c3603dfbc39c705e945d469eb253d30d
Ishu27 <36397472+Ishu27@users.noreply.github.com>
2019-08-23 10:21:55 +0530

    Update demo3.py

--- actions/demo3.py
+++ actions/demo3.py
@@ -5,7 +5,8 @@ from st2common.runners.base_action import Action
 class MyEchoAction(Action):
     def run(self, message):
         print(message)
-
-        if message == 'working':
-            return (True, message)
-        return (False, message)
+        try:
+            if message == 'working':
+                return (True, message)
+        except:
+             print("Wrong Messge")
