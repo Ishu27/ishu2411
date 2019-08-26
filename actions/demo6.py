b4356e5816ff43327133c8b9c1d0608d10b31a75
Ishu27 <36397472+Ishu27@users.noreply.github.com>
2019-08-26 17:50:29 +0530

    Update demo6.py

--- actions/demo6.py
+++ actions/demo6.py
@@ -10,7 +10,7 @@ class MyAction(Action):
 			x={"ID": id}
                 	y=json.dumps(x)
 			headers={'content-type': 'application/json'}
-			url='https://fakerestapi.azurewebsites.net/api/Books'
+			url='https://fakerestapi.azurewebsites.net/api/Books/x'
 			res=requests.get(url,headers=headers,data=y)
 		
 			z=res.json()
