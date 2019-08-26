427c158aacb5214716060c591d70c08cabda1362
Ishu27 <36397472+Ishu27@users.noreply.github.com>
2019-08-26 16:04:42 +0530

    Update demo5.py

--- actions/demo5.py
+++ actions/demo5.py
@@ -1,23 +1,21 @@
-import json
 import sys
 import requests
+import json
 
 from st2common.runners.base_action import Action
 
-class abc(Action):
-	
-	def run(self,id,title,descrp,pgcent,exercpt,publishdate):
-		
-		try:
-			x = {"ID": id, "Title":title, "Description":descrp, "PageCount":pgcent ,"Excerpt":exercpt, "PublishDate":publishdate}
-			response = requests.post("https://fakerestapi.azurewebsites.net/api/Books")
+class MyAction(Action):
+        def run(self,id,title,desc,pagecount,excerpt,pubdate):
+                try:
+              		x={"ID": id,"Title": title,"Description": desc,"PageCount": pagecount,"Excerpt": excerpt,"PublishDate": pubdate}
+                	y=json.dumps(x)
+			headers={'content-type': 'application/json'}
+			url='https://fakerestapi.azurewebsites.net/api/Books'
+			res=requests.post(url,headers=headers,data=y)
+			print(res)
+			z=res.json()
+			print(z)
 			
-			
-			
-			y = json.loads(response)
-			print(y.status_code)
-			print(json.dumps(y))
-			
-		except requests.exceptions.MissingSchema:
-			print("Wrong URL")
-			sys.exit(0)
+
+	        except:
+                    	sys.exit(0)
