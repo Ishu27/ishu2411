b1d59e5a5863f61f058864fc714dc6010a478c9e
Ishu27 <36397472+Ishu27@users.noreply.github.com>
2019-08-26 17:29:01 +0530

    Update demo6.py

--- actions/demo6.py
+++ actions/demo6.py
@@ -5,18 +5,16 @@ import json
 from st2common.runners.base_action import Action
 
 class MyAction(Action):
-        def run(self,id,title,desc,pagecount,excerpt,pubdate):
+        def run(self,id):
                 try:
-                    
-              		      x={"ID": id}
-                	      y=json.dumps(x)
-			                  headers={'content-type': 'application/json'}
-			                  url='https://fakerestapi.azurewebsites.net/api/Books'
-			                  res=requests.get(url,headers=headers,data=y)
-			                  print(res)
-			                  z=res.json()
-			                  print(z)
+			x={"ID": id}
+                	y=json.dumps(x)
+			headers={'content-type': 'application/json'}
+			url='https://fakerestapi.azurewebsites.net/api/Books'
+			res=requests.get(url,headers=headers,data=y)
+			print(res)
+			z=res.json()
+			print(z)
 			
-
-	              except:
-                    	  sys.exit(0)
+		except:
+                    	 sys.exit(0)
