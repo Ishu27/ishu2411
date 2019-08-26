f1debf3fd8f14ce304e9cc21ccfb724532527a7d
jhilmil <jhilmil123jain@gmail.com>
2019-08-26 06:23:09 +0000

    On branch master

---
+++ actions/demo5.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+
+from st2common.runners.base_action import Action
+
+class abc(Action):
+	
+	def run(self, url):
+		
+		try:
+			response = requests.get(url)
+			print(response.status_code)
+			print(response.url)
+			
+		except requests.exceptions.MissingSchema:
+			print("Wrong URL")
+			sys.exit(0)
